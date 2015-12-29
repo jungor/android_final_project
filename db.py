99e8726c92ac5963ffe6432ee83c55aff47e67d7
jungor <287150625@qq.com>
2015-12-29 17:44:48 +0800

    disable mongod auth

--- db.py
+++ db.py
@@ -6,6 +6,6 @@ import pymongo
 
 def get_db():
     client = pymongo.MongoClient()
-    client["test"].authenticate("jj", "2333")
+    # client["test"].authenticate("jj", "2333")
     db = client["test"]
     return db
