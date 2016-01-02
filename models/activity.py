45d420f5f849660f5d56426aa6b72ca82de6353c
jungor <287150625@qq.com>
2016-01-02 21:13:37 +0800

    add get acts by type api

--- models/activity.py
+++ models/activity.py
@@ -39,6 +39,7 @@ class Activity(object):
         db["Activities"].remove({})
         db["Activities"].insert(
                 {
+                    "id": 0,
                     "title": "校园跑",
                     "read_nums": "0",
                     "start_date": "2015-10-11 19:00",
@@ -46,12 +47,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "一起跑",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/0.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/0",
+                    "detail_url": SERVER_ROOT_URL + "/activity/0",
                     "type": "0",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 8,
                     "title": "校园跑",
                     "read_nums": "0",
                     "start_date": "2015-09-11 19:00",
@@ -59,12 +61,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "一起跑",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/0.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/0",
+                    "detail_url": SERVER_ROOT_URL + "/activity/8",
                     "type": "0",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 9,
                     "title": "校园跑",
                     "read_nums": "0",
                     "start_date": "2015-08-11 19:00",
@@ -72,12 +75,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "一起跑",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/0.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/0",
+                    "detail_url": SERVER_ROOT_URL + "/activity/9",
                     "type": "0",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 10,
                     "title": "校园跑",
                     "read_nums": "0",
                     "start_date": "2015-07-11 19:00",
@@ -85,12 +89,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "一起跑",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/0.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/0",
+                    "detail_url": SERVER_ROOT_URL + "/activity/10",
                     "type": "0",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 11,
                     "title": "校园跑",
                     "read_nums": "0",
                     "start_date": "2015-06-11 19:00",
@@ -98,12 +103,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "一起跑",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/0.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/0",
+                    "detail_url": SERVER_ROOT_URL + "/activity/11",
                     "type": "0",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 1,
                     "title": "三下乡",
                     "read_nums": "0",
                     "start_date": "2015-11-11 19:00",
@@ -111,12 +117,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "软件学院团委",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/1.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/1",
+                    "detail_url": SERVER_ROOT_URL + "/activity/1",
                     "type": "1",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 2,
                     "title": "认识毒药",
                     "read_nums": "0",
                     "start_date": "2015-11-11 19:00",
@@ -124,12 +131,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "软件学院学生会",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/2.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/2",
+                    "detail_url": SERVER_ROOT_URL + "/activity/2",
                     "type": "2",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 3,
                     "title": "食在广州",
                     "read_nums": "0",
                     "start_date": "2015-11-11 19:00",
@@ -137,12 +145,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "旅游协会",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/3.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/3",
+                    "detail_url": SERVER_ROOT_URL + "/activity/3",
                     "type": "3",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 4,
                     "title": "教室电影",
                     "read_nums": "0",
                     "start_date": "2015-11-11 19:00",
@@ -150,12 +159,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "科幻协会",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/4.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/4",
+                    "detail_url": SERVER_ROOT_URL + "/activity/4",
                     "type": "0",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 5,
                     "title": "3D打印前景",
                     "read_nums": "0",
                     "start_date": "2015-11-11 19:00",
@@ -163,12 +173,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "IBM俱乐部",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/5.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/5",
+                    "detail_url": SERVER_ROOT_URL + "/activity/5",
                     "type": "1",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 6,
                     "title": "净化心灵",
                     "read_nums": "0",
                     "start_date": "2015-11-11 19:00",
@@ -176,12 +187,13 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "心理学社",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/6.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/6",
+                    "detail_url": SERVER_ROOT_URL + "/activity/6",
                     "type": "2",
                 }
         )
         db["Activities"].insert(
                 {
+                    "id": 7,
                     "title": "PS讲座",
                     "read_nums": "0",
                     "start_date": "2015-11-11 19:00",
@@ -189,7 +201,7 @@ class Activity(object):
                     "sponsor": "红牛",
                     "organizer": "摄影协会",
                     "img_url": SERVER_ROOT_URL + "/static/img/activity/7.jpg",
-                    "detail_url": SERVER_ROOT_URL + "/static/img/activity/7",
+                    "detail_url": SERVER_ROOT_URL + "/activity/7",
                     "type": "3",
                 }
         )
